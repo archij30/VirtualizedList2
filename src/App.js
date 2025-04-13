6fe1123e02f82f25b045760d9ef1037b0a8ca29c
Archi Jain <archij30@gmail.com>
2025-04-13 22:08:15 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import VirtualizedList from "../VirtualizedList";
+import VirtualizedList from "./VirtualizedList";
 import "./styles.css";
 
 const ITEMS = [...Array(500).keys()];
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Virtualized List</h1>
       <VirtualizedList height={400} width={350} itemHeight={35} items={ITEMS} />
     </div>
   );
